dbe01a1a7d6a1b21bc0f35019011c10b69b03176
Justin Y. Dah-kenangnon <dah.kenangnon@gmail.com>
2020-03-09 17:36:48 +0100

    Setting... updater

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,11 @@ module.exports = {
       builderOptions: {
         // options placed here will be merged with default configuration and passed to electron-builder
         extraResources: ['src/res/'],
-        publish: ['github']
+        publish: [{
+          provider: "github",
+          owner: "EnsgmmBj",
+          repo: "AlgoEng"
+        }],
       }
     }
   }
